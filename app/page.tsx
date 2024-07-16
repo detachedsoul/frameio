d11d9acfd5ec5b73d0a9ceef0afe22fc8da892ff
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2024-07-16 20:07:29 +0100

    [INIT]: first step to greatness

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Index = (): JSX.Element => {
+    return (
+        <h1 className="font-bold text-3xl grid place-content-center h-screen">
+            First step to greatness.
+        </h1>
+    );
+};
+
+export default Index;
